406b46809fe675a9231778581775a4aa358baae6
gandatadyo <gandatadyosurya@yahoo.com>
2020-08-07 07:40:28 +0700

    Update

--- app.js
+++ app.js
@@ -1,154 +1,83 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const path = require('path')
 const fs = require('fs')
-const formidable = require('formidable')
-const pathview = path.join(__dirname + '/views')
+var multer = require('multer')
+var upload = multer({ dest: 'temp/' })
 
 const app = express()
 const port = 3000
 
-// [EJS tempalte engine]
-// app.use(express.static('views')) // static web router
-// app.use(express.static('temp'))
-// app.engine('html', ejs.renderFile);
-// app.set('view engine', 'html');
-
-// [Body Parser middleware]
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 
 const name_folder = 'temp'
 const pathfile = __dirname + "/" + name_folder;
 const testFolder = `./${name_folder}/`;
-
-
 app.use(express.static('public'))
-app.post('/getdata', async (req, res) => {
-    let search = req.query.search;
-    var PATTERN = new RegExp(search);
-    try {
-        // check folder, create if not found
-        if (!fs.existsSync(testFolder)) fs.mkdirSync(testFolder)
+app.use(express.static('name_folder'))
 
-        fs.readdir(testFolder, (err, files) => {
-            // filter base name file
+app.post('/getdata', async (req, res) => {
+    let search = req.body.search;
+    let subfolder = req.body.subfolder;
+    let PATTERN = new RegExp(search);
+    try {
+        // check folder storage, create if not found
+        testFoldertemp = testFolder
+        if (subfolder != '') {
+            testFoldertemp = testFolder + subfolder + '/'
+        }
+        if (!fs.existsSync(testFoldertemp)) fs.mkdirSync(testFoldertemp)
+
+        console.log(testFoldertemp);
+        dataset = []
+        fs.readdir(testFoldertemp, (err, files) => {
             dataList = files.filter(function (str) { return PATTERN.test(str); });
-            res.send(dataList)
-            // res.render(pathview + '/main.html', { dataList: dataList, search: search });
+            for (let i = 0; i < dataList.length; i++) {
+                let stats = fs.statSync(testFoldertemp + dataList[i]);
+                if (stats.isFile()) {
+                    dataset.push({ name: dataList[i], type: 'file' })
+                }
+                if (stats.isDirectory()) {
+                    dataset.push({ name: dataList[i], type: 'folder' })
+                }
+            }
+            res.send(dataset)
         })
     } catch (error) {
         console.log(error);
     }
 })
 
-app.post('/uploaddata', function (req, res) {
-    console.log('Upload ===> ');
-    var form = new formidable.IncomingForm();
-    form.uploadDir = pathfile
-    form.multiples = true;
-    form.parse(req, function (err, fields, files) {
-        console.log("count files :" + files.inputFile.length);
-
-        if (files.inputFile.length) {
-            // if multiple file
-            for (let index = 0; index < files.inputFile.length; index++) {
-                var tempFile = files.inputFile[index].path // this temporary file
-                var destFile = pathfile + '/' + files.inputFile[index].name
-                console.log(destFile);
-                // this procedure for move temporary to directory desctination
-                fs.rename(tempFile, destFile, (error) => {
-                    if (error) {
-                        res.send('Failed')
-                    }
-                })
+app.post('/uploaddata', upload.array('inputFile', 12), function (req, res, next) {
+    let dataobj = req.files
+    console.log(dataobj);
+    for (let i = 0; i < dataobj.length; i++) {
+        let tempFile = dataobj[i].path
+        let destFile = pathfile + '/' + dataobj[i].originalname
+        fs.rename(tempFile, destFile, (error) => {
+            if (error) {
+                res.send('Failed')
             }
-        } else {
-            // if single file
-            var tempFile = files.inputFile.path // this temporary file
-            var destFile = pathfile + '/' + files.inputFile.name
-            console.log(destFile);
-            // this procedure for move temporary to directory desctination
-            fs.rename(tempFile, destFile, (error) => {
-                if (error) {
-                    res.send('Failed')
-                }
-            })
-        }
-        res.send('success upload')
-    });
-});
+        })
+    }
+    res.send('true')
+})
 
+app.post('/deletedata', async (req, res) => {
+    let namefile = req.body.namefile
 
-// app.get('/', async (req, res) => {
-//     let search = req.query.search;
-//     var PATTERN = new RegExp(search);
-
-//     try {
-//         fs.readdir(testFolder, (err, files) => {
-//             // filter base name file
-//             dataList = files.filter(function (str) { return PATTERN.test(str); });
-//             res.render(pathview + '/main.html', { dataList: dataList, search: search });
-//         })
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// })
-
-
-app.get('/delete/:namefile', async (req, res) => {
-    let filename = req.params.namefile
-
-    filepath = testFolder + filename
+    filepath = testFolder + namefile
     if (!fs.existsSync(testFolder)) {
         fs.mkdirSync(testFolder);
     } else {
         fs.unlink(filepath, function (err) {
             if (err) throw err;
-            console.log(`File "${filename}" deleted!`);
-            res.redirect('/')
+            console.log(`File "${namefile}" deleted!`);
+            res.send('true')
         });
     }
 })
-
-// app.post('/upload', function (req, res) {
-//     var form = new formidable.IncomingForm();
-//     form.uploadDir = pathfile
-//     form.multiples = true;
-//     form.parse(req, function (err, fields, files) {
-//         console.log("count files :" + files.inputFile.length);
-
-//         if (files.inputFile.length) {
-//             // if multiple file
-//             for (let index = 0; index < files.inputFile.length; index++) {
-//                 var tempFile = files.inputFile[index].path // this temporary file
-//                 var destFile = pathfile + '/' + files.inputFile[index].name
-//                 console.log(destFile);
-//                 // this procedure for move temporary to directory desctination
-//                 fs.rename(tempFile, destFile, (error) => {
-//                     if (error) {
-//                         res.send('Failed')
-//                     }
-//                 })
-//             }
-//         } else {
-//             // if single file
-//             var tempFile = files.inputFile.path // this temporary file
-//             var destFile = pathfile + '/' + files.inputFile.name
-//             console.log(destFile);
-//             // this procedure for move temporary to directory desctination
-//             fs.rename(tempFile, destFile, (error) => {
-//                 if (error) {
-//                     res.send('Failed')
-//                 }
-//             })
-//         }
-
-//         res.redirect('/')
-//     });
-// });
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
