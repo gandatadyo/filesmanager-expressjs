2d10c66ee87c1c79d4567b1832b5d29e6903b6dc
DESKTOP-7LHU0EQ\LENOVO <gandatadyosurya@gmail.com>
2021-05-05 14:28:35 +0700

    update

---
+++ filemanager run.bat
@@ -0,0 +1,8 @@
+@echo "--> C:\Users\LENOVO\Downloads\Test1\filesmanager-expressjs" 
+
+cd C:\Users\LENOVO\Downloads\Test1\filesmanager-expressjs
+pause
+dir
+pause
+pm2 start app.js --name filemanager
+pause
